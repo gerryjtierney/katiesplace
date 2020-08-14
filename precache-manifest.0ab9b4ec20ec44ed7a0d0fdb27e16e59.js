5a6fccbbebd8283d77042be196b6cb1d8526de8d
gerryjtierney <32556699+gerryjtierney@users.noreply.github.com>
2020-08-14 17:48:45 +0100

    Updates

---
+++ precache-manifest.0ab9b4ec20ec44ed7a0d0fdb27e16e59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ef2a0850e944a4282f4b19b0facea7",
+    "url": "/katiesplace/index.html"
+  },
+  {
+    "revision": "d9f0740acf02b8061664",
+    "url": "/katiesplace/static/css/main.3602db76.chunk.css"
+  },
+  {
+    "revision": "1d4109ed5d82b4eee2d2",
+    "url": "/katiesplace/static/js/2.a0cd05a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/katiesplace/static/js/2.a0cd05a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f0740acf02b8061664",
+    "url": "/katiesplace/static/js/main.5cfcb030.chunk.js"
+  },
+  {
+    "revision": "183150f8bb3d93638522",
+    "url": "/katiesplace/static/js/runtime-main.f1de6447.js"
+  },
+  {
+    "revision": "fe6b245d31881ace122cdcd777a236f0",
+    "url": "/katiesplace/static/media/cafe.fe6b245d.ttf"
+  },
+  {
+    "revision": "8fa6d9aae57169b9cd286595ab2c66c4",
+    "url": "/katiesplace/static/media/morning-819362.8fa6d9aa.jpg"
+  },
+  {
+    "revision": "b4a6f6d03af5166994267a667cb030f9",
+    "url": "/katiesplace/static/media/pexels-negative-space-34650.b4a6f6d0.jpg"
+  },
+  {
+    "revision": "49e3145b343424c3e745274239b429e9",
+    "url": "/katiesplace/static/media/platter-2009590_1920.49e3145b.jpg"
+  }
+]);
